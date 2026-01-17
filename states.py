7d22f6f5ebb2e0f5d0d8b4b3cb398311b0f4bddb
VMhetar <mhetarvarad@gmail.com>
2026-01-17 09:41:00 +0530

    Added states.py and updated code in it.

---
+++ states.py
@@ -0,0 +1,14 @@
+from llm_base import llm_call
+from typing import Dict, List, Any
+
+class State:
+    def __init__(self, state: Dict[str, Any]):
+        self.state = state
+
+    def get_state(self)-> Dict[str, Any]:
+        return self.state
+    
+    async def update_state(self, state: Dict[str, Any],prompt:str):
+        update_data = await llm_call(prompt)
+        self.state.update(update_data)
+        return self.state
